8bc337ab0f838bf7ee498a0a3e79057009f8fac5
CarlMartimbeauETS <carl.martimbeau.1@ens.etsmtl.ca>
2022-02-10 15:22:18 -0500

    Structure de base à modifier.

--- src/laboratoire3/MotMystere.java
+++ src/laboratoire3/MotMystere.java
@@ -1,8 +1,26 @@
 package laboratoire3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.SortedSet;
+
 public class MotMystere {
     public String[] Resoudre(String nomFichierGrille, String nomFichierDict)
     {
+        //Chargé les parametres
+        char[][] grille = chargeGrille(nomFichierGrille);
+        HashMap<String, SortedSet<String>> dictionnaire = chargeDictionnaire(nomFichierDict);
+        //Résoudre le mot mystere...
+        //Inséré code ici...........
+    }
+
+    private char[][] chargeGrille(String nomFichierGrille){
+        //Chargé le dictionnaire dans un tableau NxN de caractères
+        return null;
+    }
+
+    private HashMap<String, SortedSet<String>> chargeDictionnaire(String nomFichierDict){
+        //Lire dictionnaire et mettre les différents mots
         return null;
     }
 }
