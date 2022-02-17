73b9acf31c11c88bea59da042611ddada8858c07
PierreMARUEJOL <47150800+PierreMARUEJOL@users.noreply.github.com>
2022-02-17 21:15:11 +0100

    Plateau fix

--- src/laboratoire3/MotMystere.java
+++ src/laboratoire3/MotMystere.java
@@ -5,23 +5,41 @@ import java.util.HashMap;
 import java.util.SortedSet;
 
 public class MotMystere {
-    Plateau plateau;
 
     public String[] Resoudre(String nomFichierGrille, String nomFichierDict)
     {
         Plateau plateau = new Plateau(nomFichierGrille);
         Dictionnaire dictionnaire = new Dictionnaire(nomFichierDict);
         ArrayList<String> cles = new ArrayList<>();
-        for (int i = 0; i < plateau.getTaille()-2 ; i++) {
-            for (int j = 0; j < plateau.getTaille()-2; j++) {
-                cles.add(""+plateau.grilleCases[i][j]+plateau.grilleCases[i][j+1]+ plateau.grilleCases[i][j+2]);
-                cles.add(""+plateau.grilleCases[i][j+2]+plateau.grilleCases[i][j+1]+ plateau.grilleCases[i][j]);
+        for (int i = 0; i < plateau.getTaille(); i++) {
+            for (int j = 0; j < plateau.getTaille(); j++) {
+                if (i>1){
+                    System.out.println( "East -> West");
+                }
+                if (i < plateau.getTaille()-2){
+                    System.out.println( "West - East");
+                }
+                if (j>1){
+                    System.out.println("South -> North");
+                }
+                if (j < plateau.getTaille()-2){
+                    System.out.println("North -> South");
+                }
+                if (i>1 && j>1){
+                    System.out.println("South East -> North West");
+                }
+                if (i>1 && j < plateau.getTaille()-2){
+                    System.out.println("South West -> North East");
+                }
+                if (j>1 && i < plateau.getTaille()-2){
+                    System.out.println("North East -> South West");
+                }
+                if (j < plateau.getTaille()-2 && i < plateau.getTaille()-2){
+                    System.out.println("North West -> South East");
+                }
+
             }
         }
-        for (int i = 0; i < cles.size() ; i++) {
-            System.out.println(cles.get(i));
-        }
-
         return null;
     }
 
